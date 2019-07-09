d442eacf08d69146a62a290d1a7f11337bffd995
[sandhu.navdeep938@gmail.com] <[sandhu.navdeep938@gmail.com]>
2019-07-09 14:25:46 -0400

    test

---
+++ app/src/main/java/com/example/mad_310apicall/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.example.mad_310apicall;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+public class Main2Activity extends AppCompatActivity {
+
+    ArrayList<Products> pro;
+    ListView lstv;
+
+    Listadapt adapt;
+
+
+    
